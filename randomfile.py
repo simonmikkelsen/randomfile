5cd73ef9209a3fd66e0dd5c9c6eed07087342f32
Simon Mikkelsen <simon14@zip.dk>
2017-04-13 13:53:29 +0200

    Added support for relative paths.

--- randomfile.py
+++ randomfile.py
@@ -5,12 +5,18 @@ import random
 
 class RandomFileFinderTreeWalker:
     def __init__(self):
-        pass
+        self.matched = None 
+    def get_matched_directory(self):
+        return self.matched
     def find_random_file(self, directories):
         # If we only get a single empty dir, return that.
         if len(directories) == 1 and len(os.listdir(directories[0])) == 0:
             return directories
         next_dir = random.choice(directories)
+
+        if self.matched == None:
+            self.matched = next_dir
+
         if os.path.isfile(next_dir):
             return next_dir
         else:
@@ -22,13 +28,15 @@ if __name__ == "__main__":
 
     def print_help():
         print """Usage: randomfile.py [-h] [dir ...]
-Prints the path of a random file in one of the directories given as arguments.
+Prints the full path of a random file in one of the directories given as arguments.
     -h --help      Print this message and exit.
+    -e --relative  Print the path relative to the dir argument it came from.    
     """
 
     verbose = 0
+    relative = False
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hv",
+        opts, args = getopt.getopt(sys.argv[1:], "hve",
                                    ["help", "verbose"])
     except getopt.GetoptError, err:
         print str(err)
@@ -39,6 +47,8 @@ Prints the path of a random file in one of the directories given as arguments.
             sys.exit(0)
         elif switch in ("-v", "--verbose"):
             verbose += 1
+        elif switch in ("-e", "--relative"):
+            relative = True
 
     if len(args) == 0:
         search_dirs = ['./'] 
@@ -48,7 +58,12 @@ Prints the path of a random file in one of the directories given as arguments.
 
     try:
         rand = RandomFileFinderTreeWalker()
-        print rand.find_random_file(search_dirs)
+        path = os.path.normpath(rand.find_random_file(search_dirs))
+        if relative:
+            matched_dir = os.path.normpath(rand.get_matched_directory())
+            path = path[len(matched_dir) + 1:]
+        
+        print path
         
     except KeyboardInterrupt:
         print "You cancelled."
